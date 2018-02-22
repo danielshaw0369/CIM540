ae78ff585e69bc3d945a16304f798fa76b8f082c
danielshaw0369 <danielshaw0369@gmail.com>
2018-02-21 22:19:47 -0500

    .

--- CIM540/HW/p5/hw2/sketch.js
+++ CIM540/HW/p5/hw2/sketch.js
@@ -1,4 +1,5 @@
-var hotSexyGreen = 'rgba(0,255,0, 0.25)';
+var hotSexyGreen = 'rgb(0,80,48)';
+var hotSexyOrange = 'rgb(244, 115, 33)';
 var orangeXStart = 30;
 var sideYStart = 220;
 var canvasSize = 300;
@@ -10,15 +11,15 @@ function setup() {
 }
 
 function draw() {
-  fill ('orange')
+  fill (hotSexyOrange)
   rect(orangeXStart, sideYStart, 117.5, 55, 0, 0, 20,0);
 
-  fill ('orange')
+  fill (hotSexyOrange)
   rect(orangeXStart, bottomYStart, 55, 200);
 
   fill(hotSexyGreen);
   rect(147.5, sideYStart, 117.5, 55, 0, 0, 0,20);
 
   fill(hotSexyGreen);
-  rect(210, bottomYStart, 55, 200);   
+  rect(210, bottomYStart, 55, 200);
 }
--- CIM540/HW/p5/hw3/sketch.js
+++ CIM540/HW/p5/hw3/sketch.js
@@ -1,16 +1,57 @@
+var hotSexyGreen = 'rgb(0,80,48)';
+var hotSexyOrange = 'rgb(244, 115, 33)';
+var orangeXStart = 30;
+var sideYStart = 220;
+var canvasSize = 300;
+var bottomYStart = 20;
+
+var umImage;
+var showUm = false;
+var umButton;
+var umHideButton;
+
+function preload() {
+  umImage = loadImage("assets/um.png");
+}
+
 function setup() {
-createCanvas (300, 300);
-background ('black'); // put setup code here
+  createCanvas (canvasSize, canvasSize);
+
+  background ('black');
+  umButton = createButton ("Show UM!");
+  umButton.position(175,425);
+
+  umHideButton = createButton("Hide UM!");
+  umHideButton.position(75, 425);
+
+  umButton.mousePressed(function() {
+    console.log("sick guitar solo");
+    showUm = true;
+  });
+
+  umHideButton.mousePressed(function(){
+    console.log("melee melee melee WWWARREEAAARRRHHHH");
+    showUm = false;
+  });
 }
 
 function draw() {
-fill ('orange')
-rect(30, 20, 55, 200);
-fill ('orange')
-rect(30, 220, 117.5, 55, 0, 0, 20,0);
-fill('rgba(0,255,0, 0.25)');
-rect(147.5, 220, 117.5, 55, 0, 0, 0,20);
-fill('rgba(0,255,0, 0.25)');
-rect(210, 20, 55, 200);
+  if (showUm === true){
+    clear();
+    image(umImage, 2, 50);
+  } else {
+    background ('black');
+    fill (hotSexyOrange)
+    rect(orangeXStart, sideYStart, 117.5, 55, 0, 0, 0, 0);
+
+    fill (hotSexyOrange)
+    rect(orangeXStart, bottomYStart, 55, 200);
+
+    fill(hotSexyGreen);
+    rect(147.5, sideYStart, 117.5, 55, 0, 0, 0, 0);
+
+    fill(hotSexyGreen);
+    rect(210, bottomYStart, 55, 200);
+  }
 
 }
