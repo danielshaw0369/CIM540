bc78112201bcaaec05c4f7f1d4cb994b40906405
danielshaw0369 <danielshaw0369@gmail.com>
2018-02-21 21:13:52 -0500

    .

--- CIM540/HW/p5/hw2/sketch.js
+++ CIM540/HW/p5/hw2/sketch.js
@@ -1,16 +1,24 @@
+var hotSexyGreen = 'rgba(0,255,0, 0.25)';
+var orangeXStart = 30;
+var sideYStart = 220;
+var canvasSize = 300;
+var bottomYStart = 20;
+
 function setup() {
-createCanvas (300, 300);
-background ('black'); // put setup code here
+  createCanvas (canvasSize, canvasSize);
+  background ('black'); // put setup code here
 }
 
 function draw() {
-fill ('orange')
-rect(30, 20, 55, 200);
-fill ('orange')
-rect(30, 220, 117.5, 55, 0, 0, 20,0);
-fill('rgba(0,255,0, 0.25)');
-rect(147.5, 220, 117.5, 55, 0, 0, 0,20);
-fill('rgba(0,255,0, 0.25)');
-rect(210, 20, 55, 200);
+  fill ('orange')
+  rect(orangeXStart, sideYStart, 117.5, 55, 0, 0, 20,0);
 
+  fill ('orange')
+  rect(orangeXStart, bottomYStart, 55, 200);
+
+  fill(hotSexyGreen);
+  rect(147.5, sideYStart, 117.5, 55, 0, 0, 0,20);
+
+  fill(hotSexyGreen);
+  rect(210, bottomYStart, 55, 200);   
 }
