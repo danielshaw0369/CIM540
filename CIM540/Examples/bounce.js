3d128fd828ffccbdcbd29f462b0278c5b2bce6ab
danielshaw0369 <danielshaw0369@gmail.com>
2018-03-08 12:53:48 -0500

    .

---
+++ CIM540/Examples/bounce.js
@@ -0,0 +1,19 @@
+var question = ["Is Iceland covard in ice"]
+var options = []
+var answers = ["false","atlantic"]
+function setup() {
+
+    createCanvas(400, 400);
+    console.log(options[0][1]);
+
+
+}
+
+function draw() {
+    background(255);
+    text(quaestions[currentQuestion], 20,100)
+
+
+
+
+  )
