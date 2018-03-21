b5c0fbde67891ef9d8de7816ba87f4f168e2c735
danielshaw0369 <danielshaw0369@gmail.com>
2018-03-21 00:38:09 -0400

    .

--- CIM540/HW/p5/Midterm/sketch.js
+++ CIM540/HW/p5/Midterm/sketch.js
@@ -2,7 +2,7 @@ const imageTemplate = "assets/frame_000";
 var frames = 24;
 const postfix = ".png"
 var images = [];
-var currentImage = 0;
+var currentImage = 1;
 var brokeback = "assets/Cliff.jpg";
 var mountain = {};
 
@@ -23,15 +23,16 @@ function setup() {
   var startAnimation = createButton("next image");
   startAnimation.position(width/2, 300);
   startAnimation.mousePressed(function(){
-    if (currentImage > 23);
-      currentImage = 0;
-    var next = loadImage(images[currentImage++]);
+    if (currentImage === 24)
+      currentImage = 1;
+
+    var next = loadImage(images[currentImage]);
     image(next, next.width, next.height);
+    currentImage = currentImage + 1;
   });
   mountain = loadImage(brokeback);
   console.log(mountain);
   createCanvas('2560, 1140');
-
 }
 
 function draw() {
