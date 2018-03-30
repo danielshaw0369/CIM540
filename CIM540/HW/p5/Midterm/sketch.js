55d810b2383472b5b7e90d61dd25c896e9c035d9
danielshaw0369 <danielshaw0369@gmail.com>
2018-03-30 14:48:28 -0400

    .

--- CIM540/HW/p5/Midterm/sketch.js
+++ CIM540/HW/p5/Midterm/sketch.js
@@ -6,6 +6,10 @@ var currentImage = 1;
 var brokeback = "assets/Cliff.jpg";
 var mountain = {};
 
+const framesPerSec = 4;
+var nowPlaying = false;
+var startedAt = Date.now();
+
 function preload(){
   for (var i = 1; i < frames; i++) {
     var imageWhatever = imageTemplate + i + postfix;
@@ -15,13 +19,14 @@ function preload(){
   console.log(mountain);
 
     createCanvas(1280, 480);
-    var backAnimation = createButton("previous image");
-    backAnimation.position(1080/2, 615);
+    var backAnimation = createButton("Previous Image");
+    backAnimation.position(980/2, 615);
     backAnimation.mousePressed(function(){
-      if (currentImage === 24)
+      if (currentImage === 24) {
         currentImage = 0;
-
-      currentImage = currentImage - 1;
+      } else if(currentImage > 1) {
+        currentImage = currentImage - 1;
+      }
       draw();
     });
     draw();
@@ -31,8 +36,8 @@ function preload(){
 
 function setup() {
   createCanvas(1280, 480);
-  var startAnimation = createButton("next image");
-  startAnimation.position(1280/2, 615);
+  var startAnimation = createButton("Next Image");
+  startAnimation.position(1400/2, 615);
   startAnimation.mousePressed(function(){
     if (currentImage === 24)
       currentImage = 0;
@@ -40,6 +45,20 @@ function setup() {
     currentImage = currentImage + 1;
     draw();
   });
+  var playPause = createButton("Play");
+  playPause.position(1280/2, 615);
+  playPause.mousePressed(function () {
+    nowPlaying = !nowPlaying;
+    playPause.html(nowPlaying ? "Pause" : "Play");
+    startedAt = Date.now();
+  });
+  setInterval(function () {
+    if (nowPlaying) {
+      var secs = floor((Date.now() - startedAt) / (1 / framesPerSec * 1000));
+      currentImage = secs % frames;
+      draw();
+    }
+  }, 5);
   draw();
   noLoop();
 }
