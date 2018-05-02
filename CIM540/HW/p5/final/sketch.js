61b6cd277e04da7f390238b07160d8ed36219f60
danielshaw0369 <danielshaw0369@gmail.com>
2018-05-01 20:34:16 -0400

    .

---
+++ CIM540/HW/p5/final/sketch.js
@@ -0,0 +1,27 @@
+
+var images = [];
+var wordprocessor = "assets/wp.jpg";
+var wp = {};
+
+
+function preload(){
+
+  }
+  wp = loadImage(wordprocessor);
+  console.log(wp);
+
+    createCanvas(1280, 480);
+    var start = createButton("Click to be an awesome writer");
+      }
+  }
+
+function setup() {
+  createCanvas(1280, 480);
+
+}
+
+function draw() {
+  clear();
+  background(wp);
+
+}
